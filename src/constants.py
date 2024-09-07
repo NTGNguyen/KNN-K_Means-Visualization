829e9e69c331469f240fd78c0363204357e01ff5
NTGNguyen <23521049@gm.uit.edu.vn>
2024-09-07 16:53:47 +0700

    💥 Add some constants throughtout the program

---
+++ src/constants.py
@@ -0,0 +1,33 @@
+import sys
+
+if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+    BUNDLE_DIR: str = f"{sys._MEIPASS}/"  
+else:
+    BUNDLE_DIR = ""
+
+
+BANNER = """
+██╗  ██╗███╗   ███╗███████╗ █████╗ ███╗   ██╗███████╗
+██║ ██╔╝████╗ ████║██╔════╝██╔══██╗████╗  ██║██╔════╝
+█████╔╝ ██╔████╔██║█████╗  ███████║██╔██╗ ██║███████╗
+██╔═██╗ ██║╚██╔╝██║██╔══╝  ██╔══██║██║╚██╗██║╚════██║
+██║  ██╗██║ ╚═╝ ██║███████╗██║  ██║██║ ╚████║███████║
+╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝
+        ██╗  ██╗███╗   ██╗███╗   ██╗                 
+        ██║ ██╔╝████╗  ██║████╗  ██║                 
+        █████╔╝ ██╔██╗ ██║██╔██╗ ██║                 
+        ██╔═██╗ ██║╚██╗██║██║╚██╗██║                 
+        ██║  ██╗██║ ╚████║██║ ╚████║                 
+        ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝      
+"""
+TITLE = "KNN/KMeans Clustering and Classification"
+ICON_IMG_PATH = "assets/logo.png"
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 600
+START_BUTTON_TEXT = "Start"
+RED = 'red'
+BLUE = 'blue'
+ORANGE = 'orange'
+BROWN = 'brown4'
+GREEN = 'green4'
+COLORS = [RED,BLUE,ORANGE,BROWN,GREEN]
