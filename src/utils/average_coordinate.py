0b54f9263624f6a17baa8623f6216796422b6577
NTGNguyen <23521049@gm.uit.edu.vn>
2024-09-07 16:44:03 +0700

    💥  Add utility function to calculate average coordinate

---
+++ src/utils/average_coordinate.py
@@ -0,0 +1,21 @@
+def calculate_average(coordinate_list)->list[float] | None:
+    """Function to Calculate the average of coordinate list
+
+    Args:
+        coordinate_list: list[list]
+
+    Returns:
+        List[float]: The Average Coordinate
+    """
+    if not coordinate_list:
+        return None  
+
+    
+    sum_x = sum(x for x, y in coordinate_list)
+    sum_y = sum(y for x, y in coordinate_list)
+
+    # Calculate the average
+    avg_x = sum_x / len(coordinate_list)
+    avg_y = sum_y / len(coordinate_list)
+
+    return [avg_x, avg_y]
