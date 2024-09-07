f46c1b09447f7733a905660020a0a268ef0f1b1a
NTGNguyen <23521049@gm.uit.edu.vn>
2024-09-07 21:46:21 +0700

    🎨 Add HomePage class to handle the home page functionality in the GUI

---
+++ src/GUI/screen/home_page.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+from src.constants import BANNER
+from .start_button import StartButton
+
+class HomePage(tk.Frame):
+    def __init__(self,parent,controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self,text = BANNER)
+        label.pack(pady= 20)
+
+        button = StartButton(self,controller,"MainPage")
+        button.pack()
+
+
+
+
+
