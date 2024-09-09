e0ef47fec215ac7080f3cffee092ed3285272e22
NTGNguyen <23521049@gm.uit.edu.vn>
2024-09-09 15:41:35 +0700

    ✅  Add image banner and new buttons to constants.py

--- src/constants.py
+++ src/constants.py
@@ -17,9 +17,13 @@ BANNER = """
 """
 TITLE = "KNN/KMeans Clustering and Classification"
 ICON_IMG_PATH = "assets/logo.png"
+BANNER_IMG_PATH = "assets/banner_img.png"
+BANNER_IMG_PATH_REMOVEBG = "assets/banner_img-removebg-preview.png"
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 600
 START_BUTTON_TEXT = "Start"
+NEXT_STEP_BUTTON_TEXT ="Next Step"
+DRAW_POINTS_TEXT = "Draw Points"
 RED = 'red'
 BLUE = 'blue'
 ORANGE = 'orange'
